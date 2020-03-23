2ad0e9031bb57331e56fadc8fd899a344c0045af
fersasil <gfersasil@gmail.com>
2020-03-23 18:41:46 -0300

    Fix: documentaçao adicionada

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const assistanceRoute = require('./app/routes/assistanceRoute');
+const assistanceRoutes = require('./app/routes/assistanceRoutes');
+
 const app = express();
+
+//Middlewares
 app.use(bodyParser.json());
 app.use(cors());
-app.use(assistanceRoute);
 
-const server = '192.168.0.107';
-const port = '3001';
+//Routes
+app.use(assistanceRoutes);
 
-app.listen(3001, server, () => {
-  console.log(`Server is listening on ${server}:${port}`);
+
+//Server settings
+const server = 'localhost';
+const port = '3000';
+
+app.listen(port, server, () => {
+  console.log(`Server is listening on http://${server}:${port}`);
 });
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 module.exports = mariadb.createPool({
     connectionLimit: 15,
     host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'mydb',
+    user: 'mon',
+    password: '1234',
+    database: 'myDB',
 });
